6b4cb2d13197c58ab56c782cb5a95d0c05d971fb
Eric Brown <81714784+PeanutButterRat@users.noreply.github.com>
2025-06-09 15:05:00 -0700

    Add exercises package

---
+++ cryptoserve/exercises/simple_hash.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+from cryptoserve.network import Client
+
+
+async def simple_hash(client: Client):
+    while data := (await client.recieve()):
+        hash, chunk = data[:2], data[2:]
+        hash = np.uint16(int.from_bytes(hash))
+        chunk = np.uint16(int.from_bytes(chunk))
+        hash = g(hash, chunk)
+        await client.send(int(hash).to_bytes(2))
+
+
+def g(a: np.uint16, b: np.uint16):
+    a ^= b + 0xBEAD
+    return a >> 1
--- cryptoserve/server.py
+++ cryptoserve/server.py
@@ -3,7 +3,9 @@ import asyncio
 
 import numpy as np
 
-from .network import Client
+from cryptoserve.network import Client
+from cryptoserve.greeting import GREETING, EXERCISES
+
 
 np.seterr(over="ignore")
 
@@ -51,54 +53,21 @@ async def handle_client(reader: asyncio.StreamReader, writer: asyncio.StreamWrit
 
     print(f"Accepted connection from {address}")
 
-    greeting = [
-        "=======================",
-        "Welcome to Cryptoserve!",
-        "=======================",
-        "",
-        "Available Exercises:",
-    ]
-
-    for i, (name, _) in enumerate(EXERCISES):
-        greeting.append(f"  {i}. {name}")
-
-    greeting = "\n".join(greeting)
-
     client = Client(reader, writer)
-    await client.send(greeting.encode())
+    await client.send(GREETING.encode())
+
     selection = await client.recieve()
     selection = selection.decode()
     selection = clamp(int(selection), 0, len(EXERCISES) - 1)
     exercise = EXERCISES[selection]
-    await exercise[1](client)
+
+    await exercise(client)
 
     print(f"Terminating connection with {address}")
 
 
-async def simple_hash(client: Client):
-    while data := (await client.recieve()):
-        hash, chunk = data[:2], data[2:]
-        hash = np.uint16(int.from_bytes(hash))
-        chunk = np.uint16(int.from_bytes(chunk))
-        hash = g(hash, chunk)
-        await client.send(int(hash).to_bytes(2))
-
-
-def f(a: np.uint16, b: np.uint16):
-    a ^= b + 0xC0DE
-    return (a << 3) | (a >> 13)
-
-
-def g(a: np.uint16, b: np.uint16):
-    a ^= b + 0xBEAD
-    return a >> 1
-
-
 def clamp(x, minimum, maximum):
     return max(minimum, min(x, maximum))
-
-
-EXERCISES = [("Simple Hash", simple_hash)]
 
 
 if __name__ == "__main__":
