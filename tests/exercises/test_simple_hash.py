f52330136d6319cf96f5835687572af6ad2e4907
Eric Brown <81714784+PeanutButterRat@users.noreply.github.com>
2025-06-30 18:32:38 -0700

    Add support for checking sent data

--- tests/exercises/test_simple_hash.py
+++ tests/exercises/test_simple_hash.py
@@ -4,7 +4,12 @@ from cryptoserve.exercises.simple_hash import *
 from tests.utils import run_exercise
 
 
-@run_exercise([[b"0" * 256], [b"0" * 500]])
+@run_exercise(
+    [
+        ([b"0" * 256], None),
+        ([b"0" * 500], None),
+    ]
+)
 async def test_initial_length(client):
     with pytest.raises(InvalidLengthError):
         await simple_hash(client)
@@ -12,11 +17,11 @@ async def test_initial_length(client):
 
 @run_exercise(
     [
-        ["123", "123"],
-        ["123", "123\x01"],
-        ["123", "023\x00"],
-        ["1234", "1234" + "\x00" * 3],
-        ["123", "123" + "\x00" * 5],
+        (["123", "123"], None),
+        (["123", "123\x01"], None),
+        (["123", "023\x00"], None),
+        (["1234", "1234" + "\x00" * 3], None),
+        (["123", "123" + "\x00" * 5], None),
     ]
 )
 async def test_invalid_padding(client):
@@ -26,9 +31,9 @@ async def test_invalid_padding(client):
 
 @run_exercise(
     [
-        ["Apple", "Apple" + "\x00" * 3, b"\x00\x00"],
-        ["Apple", "Apple" + "\x00" * 3, b"\x3a\x59"],
-        ["Apple", "Apple" + "\x00" * 3, b"\x3a\x58", b"\x7b\xf0"],
+        (["Apple", "Apple" + "\x00" * 3, b"\x00\x00"], None),
+        (["Apple", "Apple" + "\x00" * 3, b"\x3a\x59"], None),
+        (["Apple", "Apple" + "\x00" * 3, b"\x3a\x58", b"\x7b\xf0"], None),
     ]
 )
 async def test_invalid_hash(client):
@@ -38,7 +43,10 @@ async def test_invalid_hash(client):
 
 @run_exercise(
     [
-        ["Apple", "Apple" + "\x00" * 3, b"\x3a\x58", b"\x7b\xf1"],
+        (
+            ["Apple", "Apple" + "\x00" * 3, b"\x3a\x58", b"\x7b\xf1"],
+            ["OK", "OK", b"\x0a\xa0", b"\x62\xae"],
+        ),
     ]
 )
 async def test_successful_completion(client):
--- tests/utils.py
+++ tests/utils.py
@@ -6,10 +6,17 @@ import pytest
 from cryptoserve.messaging import Client
 
 
-def create_mock_client(recieved_data: list[bytes | str]):
-    recieved_data = list(
-        map(lambda arg: arg.encode() if isinstance(arg, str) else arg, recieved_data)
-    )
+def wrap_data(data: bytes) -> bytes:
+    data_length = len(data)
+    header = bytearray(data_length.to_bytes(2))
+    return header + data
+
+
+def create_mock_client(recieved_data, sent_data) -> Client:
+    recieved_data = [
+        data.encode() if isinstance(data, str) else data for data in recieved_data
+    ]
+
     forbidden_mock = AsyncMock(
         side_effect=RuntimeError(
             "directly accessing the StreamReader or StreamWriter during a test is not allowed"
@@ -17,17 +24,39 @@ def create_mock_client(recieved_data: list[bytes | str]):
     )
     mock_client = Client(forbidden_mock, forbidden_mock)
     mock_client._recieve = AsyncMock(side_effect=recieved_data)
-    mock_client._send = AsyncMock()
+    assertion_index = [0]
+
+    if sent_data is not None:
+        sent_data = [
+            data.encode() if isinstance(data, str) else data for data in sent_data
+        ]
+        sent_data = [wrap_data(data) for data in sent_data]
+
+        async def mock_send(data: bytes):
+            index = assertion_index[0]
+            expected = sent_data[index]
+            assert (
+                data == expected
+            ), f"actual data sent on call {index} ({data}) does not match expected data sent ({expected})"
+            assertion_index[0] += 1
+
+        mock_client._send = mock_send
+    else:
+        mock_client._send = AsyncMock()
 
     return mock_client
 
 
-def run_exercise(recieved_data: list[list[bytes]]):
+def run_exercise(test_data: list[tuple[list[bytes | str], list[bytes | str] | None]]):
     def decorator(test: Callable):
+        mock_clients = []
+
+        for recieved_data, sent_data in test_data:
+            mock_client = create_mock_client(recieved_data, sent_data)
+            mock_clients.append(mock_client)
+
         test = pytest.mark.asyncio(test)
-        test = pytest.mark.parametrize(
-            "client", [create_mock_client(data) for data in recieved_data]
-        )(test)
+        test = pytest.mark.parametrize("client", mock_clients)(test)
         return test
 
     return decorator
